bc9143635c1110ffd27f3ad1c5f8e9bba3e5fcc1
Lucas <lucaspereiracunha08@gmail.com>
2018-10-15 23:12:52 -0500

    updated page with 3 API calls

--- js/main.js
+++ js/main.js
@@ -11,7 +11,7 @@ $(document).ready(function () {
         // console.log(randNumAppend);
         // console.log(innervalue);
         if (randNumAppend == innervalue) {
-            var appendList = '<div class="removeOnConfirm2"> <h1 class="display-3">Reward your farmer</h1><p class="lead">Customer reviews </p> <div class="rating"> <span>☆</span> <span>☆</span> <span>☆</span> <span>☆</span> <span>☆</span> </div> <br> <button class="submitReview">Submit your review</button></div>';
+            var appendList = '<div class="removeOnConfirm2"> <h1 class="display-3">Reward your farmer</h1><p class="lead">Share your stars with the farmer and receive free HBars</p> <div class="rating"> <span>☆</span> <span>☆</span> <span>☆</span> <span>☆</span> <span>☆</span> </div> <br> <button class="submitReview">Submit your review</button></div>';
             $('.removeOnConfirm').remove();
             $('.carousel-caption').append(appendList);
         } else {
@@ -21,28 +21,85 @@ $(document).ready(function () {
 
     // Sends AJAX post request
     $('body').on('click', '.submitReview', function () {
-        // $.ajax({
-        //     url: '/api/transfer',
-        //     data: {
-        //         from: '',
-        //         to: '',
-        //         amount: ''
-        //     },
-        //     method: "POST",
-        // }).done(function (msg) {
-        //     alert("Data Saved: " + msg);
-        // });
-        //curl --request POST 'http://localhost:8080/api/transfer' --header "Content-Type:application/json" --header "Accept:application/json" --data '{"data":{"to":"1001", "from": "1002", "amount": 1}}'
+        var HBarReceived = '1000';
+        var settings = {
+            "async": true,
+            "crossDomain": true,
+            "url": "http://localhost:8080/api/transfer",
+            "method": "POST",
+            "headers": {
+                "Content-Type": "application/json",
+                "Cache-Control": "no-cache",
+                "Postman-Token": "87fd947e-dac7-47da-8ab4-966cc90a14e9"
+            }
+        }
+
+        $.ajax(settings).done(function (response) {
+            console.log(response);
+            // alert(response);
+            // var HBarTotal = JSON.stringify(response.toAccountTotal);
+            // var test = JSON.parse(response);
+            var HBarTotal = response;
+            console.log(response);
+            // var HBarTotal = '12312312';
+            var appendList2 = '<div class="newDiv"><p class="thankYou">Thank you for your review</><div class="userReward"><p>Your Reward: </p> <p class="totalHbarText">$<span>' + HBarReceived + '</span> HBars have been added to your account</p> <div class="getBalanceWrapper"><p class="totalHbarText2">Click to get your total balance: </p><button class="getBalance">get balance</button></div><p class="totalHbarText2">How much would you like to tip your farmer for?</p> <input type="text" id="inputValue2"> <br><button class="submitTip">Tip your farmer</button></div> </div>'
+            $('.removeOnConfirm2').remove();
+            $('.carousel-caption').append(appendList2);
+        });
+
         // // Receives answer from Ajax and display to user
         // if(transaction is successfull)
-        // var appendList2 = '<p>Pasture to Plate has received your transaction successfully</p><br><p>Message from the farmer:</p><br><p>I am very happy to be able to share my product information with you. Hope you love eaiting your food the same way we love preparing it for you!</p>'
-        var appendList2 = '<div class="newDiv"><p class="thankYou">Thank you for your review</><div class="farmerReward"><p>Farmer Reward: </p></div><div class="userReward"><p>Your Reward: </p> <br> <select class="tipOption"><option>15%</option><option>18%</option><option>20%</option></select><br><button class="submitTip">Tip your farmer</button></div> </div>'
-        $('.removeOnConfirm2').remove();
-        $('.carousel-caption').append(appendList2);
+
         $('.carousel-caption').css({
             'display': 'grid !important',
             'grid-template-columns': '1fr 1fr',
             'grid-template-rows': '0.2fr'
         });
+    })
+    $('body').on('click', '.getBalance', function () {
+        var settings = {
+            "async": true,
+            "crossDomain": true,
+            "url": "http://localhost:8080/api/getBalance",
+            "method": "POST",
+            "headers": {
+                "Content-Type": "application/json",
+                "Cache-Control": "no-cache",
+                "Postman-Token": "74b226f9-2358-40d2-b2d4-1d6dcdb101e9"
+            },
+            "processData": false,
+            "data": "{\"data\": {\"accountId\": 123}}"
+        }
+
+        $.ajax(settings).done(function (response) {
+            console.log(response);
+            // var test = JSON.parse(response);
+            var updatedBalance = response;
+            var newBalance = '<p class="totalHbarText2">Your Balance Is: <span>' + updatedBalance + '</span></p>'
+            $('.getBalanceWrapper').remove();
+            $('.totalHbarText').append(newBalance);
+            var HBarReceived = '1000';
+            var settings = {
+                "async": true,
+                "crossDomain": true,
+                "url": "http://localhost:8080/api/transfer",
+                "method": "POST",
+                "headers": {
+                    "Content-Type": "application/json",
+                    "Cache-Control": "no-cache",
+                    "Postman-Token": "87fd947e-dac7-47da-8ab4-966cc90a14e9"
+                }
+            }
+
+            $.ajax(settings).done(function (response) {
+                console.log(response);
+            })
+        });
+
+    })
+    $('body').on('click', '.submitTip', function () {
+        var appendList3 = '<p>Pasture to Plate has received your tip</p><br><p>Message from the farmer:</p><br><p>I am very happy to be able to share my product information with you. Hope you love eaiting your food the same way we love preparing it for you!</p>'
+        $('.newDiv').remove();
+        $('.carousel-caption').append(appendList3);
     })
 })
